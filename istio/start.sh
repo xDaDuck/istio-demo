3823a7d7b592db3bdd4be7452f2d0e246dcf91ba
dcapozzi <davide.capozzi@objectway.it>
2020-01-31 12:52:44 +0100

    fix configs

--- istio/start.sh
+++ istio/start.sh
@@ -3,14 +3,40 @@
 basedir=$(dirname $0)
 rootdir=${basedir}/..
 
+if [[ "$1" == "-m" ]]; then
+  echo "setup microk8s kubectl & istioctl"
+  kubectl() {
+    microk8s.kubectl $@
+  }
+
+  istioctl() {
+    microk8s.istioctl $@
+  }
+fi
+
 istio-apply(){
-        kubectl apply -f <( istioctl kube-inject -f $@ )
+  kubectl apply -f <( istioctl kube-inject -f $@ )
 }
 
-istio-apply ${basedir}/demo-v1.yaml
-istio-apply ${basedir}/sleep.yaml
+kubectl delete -f sleep.yaml
+kubectl delete -f demo.yaml
+kubectl delete -f demo-istio.yaml
+kubectl delete -f proxy.yaml
+kubectl delete -f proxy-istio.yaml
 
-istio-apply ${basedir}/demo-sa.yaml
-istio-apply ${basedir}/demo-svc.yaml
-istio-apply ${basedir}/demo-vs.yaml
-istio-apply ${basedir}/demo-dr.yaml
+istio-apply ${basedir}/sleep.yaml
+istio-apply ${basedir}/demo.yaml
+
+kubectl apply -f demo-istio.yaml
+kubectl apply -f proxy.yaml
+
+PROXY_PORT=3128
+
+while [[ -z "$PROXY_IP" ]]; do
+  PROXY_IP=$(kubectl get pod -n external -l app=squid -o jsonpath={.items..podIP})
+done
+
+cat proxy-istio.yaml | sed -r "s/<ADDRESS>/$PROXY_IP/" | microk8s.kubectl apply -f-
+
+echo -e "\e[33madd proxy"
+echo -e "export PROXY=${PROXY_IP}:${PROXY_PORT}\e[0m"
